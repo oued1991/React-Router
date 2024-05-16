e8e9d7134041e9a6ff6e8e96e9cb8311c37b8162
oued1991 <o_abass40@yahoo.com>
2024-05-15 21:41:20 -0400

    Complete React Router Tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,61 @@
 import React from 'react'
-import Root from "./routes/root";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import ReactDOM from 'react-dom/client'
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
+import { action as destroyAction } from "./routes/destroy";
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css';
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
